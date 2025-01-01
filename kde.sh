2e63a4b9b97ae36fc57305e494671f1081040a49
Shinigami-98 <shinigami-sama.98@proton.me>
2025-01-01 22:42:58 +0545

    Create kde.sh

---
+++ kde.sh
@@ -0,0 +1,6 @@
+
+sudo pacman -S fastfetch discover dolphin ark bluez firewalld kde-gtk-config breeze-gtk kdeconnect kdeplasma-addons bluedevil kgamma kscreen plasma-firewall plasma-browser-integration plasma-nm plasma-pa plasma-sdk plasma-systemmonitor power-profiles-daemon kwallet-pam
+
+sudo pacman -S ttf-liberation ttf-dejavu noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-roboto ttf-roboto-mono ttf-droid ttf-opensans ttf-hack ttf-fira-code ttf-fira-mono ttf-cascadia-code adobe-source-code-pro-fonts adobe-source-sans-fonts adobe-source-serif-fonts os-prober micro git wget bluez pipewire
+
+
